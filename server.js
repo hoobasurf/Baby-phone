a111339c495aa0e69eac7f08617f1301acecd6e5
hoobasurf <brice.paneela@icloud.com>
2025-11-05 22:37:33 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
 const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
 const app = express();
-const server = require("http").createServer(app);
-const io = require("socket.io")(server, {
-  cors: { origin: "*" }
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: { origin: "*" } // autorise toutes les origines pour Netlify
 });
 
-io.on("connection", socket => {
-  console.log("Nouvelle connexion:", socket.id);
+// Vérification serveur
+app.get("/", (req, res) => res.send("Babyphone serveur actif"));
 
-  socket.on("offer", data => io.to(data.target).emit("offer", { sdp: data.sdp, from: socket.id }));
-  socket.on("answer", data => io.to(data.target).emit("answer", { sdp: data.sdp }));
-  socket.on("ice-candidate", data => io.to(data.target).emit("ice-candidate", { candidate: data.candidate }));
-  socket.on("join", room => { socket.join(room); io.to(room).emit("user-joined", socket.id); });
+// Stocke temporairement les blobs audio
+io.on("connection", (socket) => {
+    console.log("Client connecté :", socket.id);
+
+    // Quand le bébé envoie un blob audio
+    socket.on("audio-stream", (data) => {
+        // Renvoie à tous les autres clients (parents)
+        socket.broadcast.emit("audio-stream", data);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("Client déconnecté :", socket.id);
+    });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server démarré sur port ${PORT}`));
+// Démarrage serveur
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => console.log(`Serveur Babyphone démarré sur port ${PORT}`));
